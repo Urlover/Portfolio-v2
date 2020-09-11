68f543a11d89f77e90c125c5d523956203764073
UrLover <jonah_louis@outlook.com>
2020-09-11 00:42:56 -0400

    Updated steam link to open new tab

--- social.php
+++ social.php
@@ -2,6 +2,6 @@
     <div class="vl"></div>  <!-- vertical line #1 -->
     <a href="mailto:jonah_louis@outlook.com"><i class="fas fa-envelope fa-2x"></i></a>
     <a href="https://github.com/Urlover" target="_blank"><i class="fab fa-github fa-2x"></i></a>
-    <a href="https://steamcommunity.com/id/imurl0ver/"><i class="fab fa-steam fa-2x"></i></a>
+    <a href="https://steamcommunity.com/id/imurl0ver/" target="_blank"><i class="fab fa-steam fa-2x"></i></a>
     <div class="vl"></div>  <!-- vertical line #2 -->
 </div>
