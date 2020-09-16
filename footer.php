b84f366b1d69ecb3b7bd35f33cd5eaa07f4f6f79
UrLover <jonah_louis@outlook.com>
2020-09-16 15:34:55 -0400

    disabled footer button

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
  <!-- Footer -->
 <div class="footer">
-    <a href="#content" data-toggle="tooltip" data-placement="top" title="Go to top"><i class="far fa-arrow-alt-circle-up fa-2x" ></i></a>
+    <a href="#" data-toggle="tooltip" data-placement="top" title="Go to top"><i class="far fa-arrow-alt-circle-up fa-2x" ></i></a>
 </div>
