741babe245212760c76bc0a534b5095c1da2fe88
Jonah Louis <jonah_louis@outlook.com>
2020-10-02 15:06:14 -0400

    Removed current link on 404.php

--- nav.php
+++ nav.php
@@ -14,10 +14,13 @@
         if (stripos($_SERVER['SCRIPT_NAME'], 'index.php')) {
           echo '<li class="nav-item active pr-4"><a id="navbar-curr" href="index.php">About Me</a></li>';
           echo '<li class="nav-item pr-4"><a href="projects.php">Projects</a></li> ';
-        } else {
+        } else if (stripos($_SERVER['SCRIPT_NAME'], 'projects.php')) {
           echo '<li class="nav-item active pr-4"><a href="index.php">About Me</a></li>';
           echo '<li class="nav-item pr-4"><a id="navbar-curr" href="projects.php">Projects</a></li> ';
-        } 
+        } else {
+          echo '<li class="nav-item active pr-4"><a href="index.php">About Me</a></li>';
+          echo '<li class="nav-item pr-4"><a href="projects.php">Projects</a></li> ';
+        }
         ?>
       </ul>
       <!-- Links & buttons on the right -->  
