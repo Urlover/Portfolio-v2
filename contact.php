24fa55bb28a71c2fac0a5f8f1e3d6c50cd9f3071
UrLover <jonah_louis@outlook.com>
2020-10-18 19:28:13 -0400

    Added mail function to contact form

--- contact.php
+++ contact.php
@@ -33,7 +33,7 @@
             <h1 class="uni-mainHeader">Contact Me</h1>
         </div>
         <div class="contact-form-box">
-            <form>
+            <form method="post">
                 <div class="row">
                     <div class="col-md-6 form-input">
                         <input type="text" id="fName" name="firstName" placeholder="First Name">
@@ -42,8 +42,11 @@
                         <input type="text" id="lName" name="lastName" placeholder="Last Name">
                     </div>
                     <div class="col-sm-12 form-input">
-                        <input type="text" id="subject" name="subject" placeholder="Subject">
+                        <input type="text" id="email" name="email" placeholder="Email">
                     </div>
+                    <div class="col-sm-12 form-input">
+                        <input type="text" id="subject" name="subject" placeholder="Subject">
+                    </div>  
                     <div class="col-sm-12 form-input">
                         <textarea id="message" name="message" placeholder="Message"></textarea>
                     </div>
@@ -54,6 +57,35 @@
             </form>
         </div>
     </div>
+
+    <?php 
+        // Used to send email to jonah_louis@outlook.com
+         if ( !empty($_POST) ) {
+            /* Varible initialization & declaration */
+            // My email
+            $myEmail = "jonah_louis@outlook.com";
+
+            // User information
+            $firstName = $_POST["firstName"];
+            $lastName = $_POST["lastName"];
+            $subject = $_POST["subject"];
+            $message = wordwrap($_POST["message"], 70);
+            $email = $_POST["email"];
+
+            // Formatted information
+            $fSubject = $subject . ": jonahlouis.ca form submission";   // subject for my message
+            $fSubject2 = "Form submitted from jonahlouis.ca";           // subject for sender
+            $fMessage = "First Name:" . $firstName . "\nLast Name:" . $lastName . "\nMessage:\n" . $message;    // message to me
+            $fMessage2 = "Hi" . $firstName .",\nThis is a confirmation email for your form submission on jonahlouis.ca.The following message was sent:\n"
+            ."First Name:" . $firstName ."\nLast Name:" . $lastName ."\nMessage:\n" . $message;    // message to sender (copy)
+
+            // Send emails
+            mail($myEmail, $fSubject, $fMessage);
+            mail($email, $fSubject2, $fMessage2);
+
+            echo "Success!";
+        }
+    ?>
         
     <!-- Footer -->
     <?php //include("footer.php"); ?>
