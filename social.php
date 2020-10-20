a1cf9a5e8604fdeb6dad727a56fa78f8519195c1
UrLover <jonah_louis@outlook.com>
2020-10-20 16:42:20 -0400

    Mail icon now redirects to contact form

--- social.php
+++ social.php
@@ -1,6 +1,6 @@
 <div class="social-bar fade-in-social">
     <div class="vl"></div>  <!-- vertical line #1 -->
-    <a href="mailto:jonah_louis@outlook.com"><i class="fas fa-envelope fa-2x"></i></a>
+    <a href="contact.php"><i class="fas fa-envelope fa-2x"></i></a>
     <a href="https://github.com/jonahlouis4" target="_blank"><i class="fab fa-github fa-2x"></i></a>
     <a href="https://steamcommunity.com/id/imurl0ver/" target="_blank"><i class="fab fa-steam fa-2x"></i></a>
     <div class="vl"></div>  <!-- vertical line #2 -->
